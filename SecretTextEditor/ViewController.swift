59134adc5d45ffb5762a1742552af418b588f9aa
NickSagan <9952728@gmail.com>
2021-12-01 18:39:47 +0300

    Initial Commit

---
+++ SecretTextEditor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecretTextEditor
+//
+//  Created by Nick Sagan on 01.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
